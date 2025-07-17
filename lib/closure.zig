4bb3a5e68462a032df542c7c25c7fc94339e7a4a
Tristan Ross <tristan.ross@midstall.com>
2025-07-17 15:14:54 -0700

    feat: add impl closure

--- lib/closure.zig
+++ lib/closure.zig
@@ -1,6 +1,45 @@
 const std = @import("std");
 
-pub fn Closure(comptime Function: anytype) type {
+pub fn ImplClosure(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        args: Arguments,
+        func: T,
+
+        pub const Arguments = blk: {
+            const params = @typeInfo(T).@"fn".params;
+
+            var fields: [params.len]std.builtin.Type.StructField = undefined;
+            for (params, &fields, 0..) |p, *field, i| {
+                @setEvalBranchQuota(10_000);
+                var num_buf: [128]u8 = undefined;
+                field.* = .{
+                    .name = std.fmt.bufPrintZ(&num_buf, "{d}", .{i}) catch unreachable,
+                    .type = p.type orelse unreachable,
+                    .default_value_ptr = null,
+                    .alignment = 0,
+                    .is_comptime = false,
+                };
+            }
+
+            break :blk @Type(.{ .@"struct" = .{
+                .layout = .auto,
+                .decls = &.{},
+                .fields = &fields,
+                .is_tuple = true,
+            } });
+        };
+
+        pub const Result = @typeInfo(T).@"fn".return_type orelse unreachable;
+
+        pub fn run(self: *const Self) Result {
+            return @call(.auto, self.func, self.args);
+        }
+    };
+}
+
+pub fn FixedClosure(comptime Function: anytype) type {
     return struct {
         const Self = @This();
         const T = @TypeOf(Function);
@@ -39,8 +78,12 @@ pub fn Closure(comptime Function: anytype) type {
     };
 }
 
-pub fn closure(comptime Function: anytype, args: anytype) Closure(Function) {
-    return Closure(Function){ .args = args };
+pub fn implClosure(comptime func: anytype, args: anytype) ImplClosure(@TypeOf(func)) {
+    return ImplClosure(@TypeOf(func)){ .args = args, .func = func };
+}
+
+pub fn fixedClosure(comptime Function: anytype, args: anytype) FixedClosure(Function) {
+    return FixedClosure(Function){ .args = args };
 }
 
 test {
@@ -58,7 +101,11 @@ test {
         }
     };
 
-    try std.testing.expectEqual(@as(u8, 19), closure(TestFunctions.add, .{ 9, 10 }).run());
-    try std.testing.expectEqual(@as(u8, 9), closure(TestFunctions.sub, .{ 19, 10 }).run());
-    try std.testing.expectError(error.Unexpected, closure(TestFunctions.doError, .{}).run());
+    try std.testing.expectEqual(@as(u8, 19), implClosure(TestFunctions.add, .{ 9, 10 }).run());
+    try std.testing.expectEqual(@as(u8, 9), implClosure(TestFunctions.sub, .{ 19, 10 }).run());
+    try std.testing.expectError(error.Unexpected, implClosure(TestFunctions.doError, .{}).run());
+
+    try std.testing.expectEqual(@as(u8, 19), fixedClosure(TestFunctions.add, .{ 9, 10 }).run());
+    try std.testing.expectEqual(@as(u8, 9), fixedClosure(TestFunctions.sub, .{ 19, 10 }).run());
+    try std.testing.expectError(error.Unexpected, fixedClosure(TestFunctions.doError, .{}).run());
 }
